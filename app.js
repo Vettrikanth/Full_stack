3ec48845d8d8ed21a7e4c60a783ee3607a1621e8
Vettri <msvettrikanth@gmail.com>
2023-08-03 11:32:43 +0530

    js

---
+++ app.js
@@ -0,0 +1,25 @@
+// var a=120
+// console.log("value in var type:",a)
+// {
+//     var a=2
+//     console.log("after altering",a)
+// }
+// console.log("outside block",a)
+
+// let a=120
+// console.log("value in let type:",a)
+// {
+//     let a=7
+//     console.log("value in let after altering",a)
+// }
+// console.log("value in let outside block",a)
+
+// let b=200
+// console.log("value in let type:",b)
+// const c=20
+// console.log("value in const type:",c)
+
+const c=100
+console.log(c)
+const cc=56
+console.log(cc)
