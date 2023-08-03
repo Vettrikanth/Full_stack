c1e3ff9e1b794ebc77cec1e696315209cae6fc4d
Vettri <msvettrikanth@gmail.com>
2023-08-03 16:38:54 +0530

    js

--- link.js
+++ link.js
@@ -1,3 +1,9 @@
 function myfunhome(){
-    document.getElementById("hello1").innerHTML="hii theree"
+    document.getElementById("hello1").innerHTML="how r u"
+}
+function myfunhome2(){
+    document.getElementById("hello1").innerHTML="1253356"
+}
+function myfunhome3(){
+    document.getElementById("hello1").innerHTML="kct cbe"
 }
