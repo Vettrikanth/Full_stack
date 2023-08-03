24b92c7867d548b27f713daa78ebb7ca4e68eabb
Vettri <msvettrikanth@gmail.com>
2023-08-03 12:22:38 +0530

    js

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
-// var a=120
-// console.log("value in var type:",a)
-// {
-//     var a=2
-//     console.log("after altering",a)
-// }
-// console.log("outside block",a)
+var a=120
+console.log("value in var type:",a)
+{
+    var a=2
+    console.log("after altering",a)
+}
+console.log("outside block",a)
 
-// let a=120
-// console.log("value in let type:",a)
-// {
-//     let a=7
-//     console.log("value in let after altering",a)
-// }
-// console.log("value in let outside block",a)
+let a2=120
+console.log("value in let type:",a2)
+{
+    let a2=7
+    console.log("value in let after altering",a2)
+}
+console.log("value in let outside block",a2)
 
-// let b=200
-// console.log("value in let type:",b)
-// const c=20
-// console.log("value in const type:",c)
-
-// const c=100
-// console.log(c)
-// const cc=56
-// console.log(cc)
+let b=200
+console.log("value in let type:",b)
 
 
-// a=4 
-// if (a===0){
-//     console.log("Zero")
-// }
-// else if(a>0){
-//     console.log("Positive")
-// }
-// else{
-//     console.log("Negative")
-// }
+const c=100
+console.log(c)
+const cc=56
+console.log(cc)
 
 
-a=10
-for (i=1;i<a;i++){
-    console.log(i)
+a3=4 
+if (a3===0){
+    console.log("Zero")
+}
+else if(a3>0){
+    console.log("Positive")
+}
+else{
+    console.log("Negative")
 }
 
 
+a4=10
+for (i=1;i<a4;i++){
+    console.log(i)
+}
+console.log("\n")
 i=1
-while (i<a){
+while (i<a4){
     console.log(i)
     i++
 }
+
+console.log("\n")
+n=1
+l1=[100,200,300,400,500]
+l1.forEach(i => {
+    console.log("elemnt",n)
+    console.log(i)
+    n++
+});
